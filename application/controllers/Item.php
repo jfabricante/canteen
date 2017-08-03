043b14459e64026eb1535fe07cb72a2931a59286
Jerome <jerome-fabricante@isuzuphil.com>
2017-08-03 12:57:01 +0800

    Create item controller

--- application/controllers/Item.php
+++ application/controllers/Item.php
@@ -6,25 +6,67 @@ class Item extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+
+		$this->load->helper('form');
+
+		$this->load->model('item_model', 'item');
+		$this->load->model('category_model', 'category');
 	}
 
-	public function items()
+	public function list_()
 	{
 		$data = array(
 				'title'   => 'List of Items',
-				'content' => 'items/items_view'
+				'content' => 'item/list_view',
+				'items'   => $this->item->browse()
 			);
 
 		$this->load->view('include/template', $data);
 	}
 
-	public function item_form()
+	public function form()
 	{
+		$id = $this->uri->segment(3) ? $this->uri->segment(3) : 0;
+
 		$data = array(
-				'title' => 'Item form',
-				'content' => 'items/item_form_view',
+				'title'      => $id ? 'Update details' : 'Item form',
+				'content'    => 'item/form_view',
+				'entity'     => $id ? $this->item->read(array('id' => $id, 'type' => 'object')) : '',
+				'categories' => $this->category->browse()
 			);
 
-		$this->load->view('include/template', $data);
+		$this->load->view('item/form_view', $data);
+	}
+
+	public function store()
+	{
+		$id = $this->input->post('id');
+		var_dump($this->input->post());
+		/*$data = $this->_handle_upload();
+
+		if (is_array($data))
+		{
+
+		}*/
+	}
+
+	protected function _handle_upload()
+	{
+		$config = array(
+				'upload_path'   => './resources/thumbnail',
+				'allowed_types' => 'gif|jpg|png',
+				'max_size'      => 100
+			);
+
+		$this->load->library('upload', $config);
+
+		if (!$this->upload->do_upload('thumbnail'))
+		{
+			$error = array('error' => $this->upload->display_errors());
+
+			return $this->upload->display_errors();
+		}
+
+		return $this->upload->data();
 	}
 }
