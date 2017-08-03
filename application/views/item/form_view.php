9baac1a2d09601445a25ceab705b473bee589b79
Jerome <jerome-fabricante@isuzuphil.com>
2017-08-03 13:05:04 +0800

    Item list view
    Item form

---
+++ application/views/item/form_view.php
@@ -0,0 +1,56 @@
+<?php echo form_open_multipart('item/store');?>
+<!-- <form action="<?php //echo base_url('index.php/item/store'); ?>" method="post" enctype="multipart/form-data"> -->
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">×</span>
+		</button>
+		<h4 class="modal-title"><?php echo $title; ?></h4>
+	</div>
+
+	<div class="modal-body">
+		<div class="form-group hidden">
+			<input type="text" class="form-control" id="id" name="id" value="<?php echo isset($entity->id) ? $entity->id : 0; ?>" />
+		</div>
+
+		<div class="form-group">
+			<label for="name">Item name</label>
+			<input type="text" class="form-control" id="name" name="name" value="<?php echo isset($entity->name) ? $entity->name : ''; ?>" required />
+		</div>
+
+		<div class="form-group">
+			<label for="price">Price</label>
+			<input type="number" class="form-control" id="price" name="price" value="<?php echo isset($entity->price) ? $entity->price : ''; ?>" required />
+		</div>
+
+		<div class="form-group">
+			<label for="thumbnail">Thumbnail</label>
+			<input type="file" class="form-control" id="thumbnail" name="thumbnail" accept="image/*" value="<?php echo isset($entity->thumbnail) ? $entity->thumbnail : ''; ?>" />
+
+		</div>
+
+		<div class="form-group">
+			<label for="category_id">category</label>
+			<select name="category_id" id="category_id" class="form-control select2" data-live-search="true" required>
+				<option></option>
+				<?php foreach($categories as $category): ?>
+					<option value="<?php echo $category->id; ?>" <?php echo isset($entity->category_id) ? $category->id == $entity->category_id ? 'selected' : '' : ''; ?> ><?php echo $category->name; ?></option>
+				<?php endforeach; ?>
+			</select>
+		</div>
+	</div>
+	
+	<div class="modal-footer">
+		<div class="form-group">
+			<button type="button" class="btn btn-flat btn-info pull-left" data-dismiss="modal">Close</button>
+			<input type="submit" value="Submit" class="btn btn-flat btn-danger">
+		</div>
+	</div>
+	
+</form><!-- End Form -->
+<!-- End of table -->
+				
+<script type="text/javascript">
+	$(document).ready(function() {
+		//$('.table').DataTable();
+	});
+</script>
---
+++ application/views/item/list_view.php
@@ -0,0 +1,84 @@
+<?php var_dump($this->session->userdata()); ?>
+<!-- Items block -->
+<section class="content items">
+	<!-- row -->
+	<div class="row">
+		<!-- col-md-6 -->
+		<div class="col-md-6">
+			<!-- Box danger -->
+			<div class="box box-danger">
+				<!-- Content -->
+				<div class="box-header with-border">
+					<a href="<?php echo base_url('index.php/item/form') ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
+						<button class="btn btn-flat btn-success pull-right">Add Item <i class="fa fw fa-plus" aria-hidden="true"></i></button>
+					</a>
+				</div>
+
+				<div class="box-body">
+					<!-- Item table -->
+					<table class="table table-condensed table-striped table-bordered">
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Name</th>
+								<th>Price</th>
+								<th>Category</th>
+								<th>Thumbnail</th>
+								<th>Date Time</th>
+								<th>Edit</th>
+								<th>Delete</th>
+							</tr>
+						</thead>
+
+						<tbody>
+							<?php $count = 1; ?>
+							<?php foreach($items as $item): ?>
+								<tr>
+									<td><?php echo $count ?></td>
+									<td><?php echo $item->name; ?></td>
+									<td><?php echo $item->price; ?></td>
+									<td><?php echo $item->category; ?></td>
+									<td><?php echo $item->thumbnail; ?></td>
+									<td><?php echo date('m/d/Y h:i A', strtotime($item->datetime)); ?></td>
+									<td>
+										<a href="<?php echo base_url('index.php/item/form/' . $item->id); ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
+											<i class="fa fa-pencil" aria-hidden="true"></i>
+										</a>
+									</td>
+									<td>
+										<a href="<?php echo base_url('index.php/item/notice/' . $item->id); ?>" data-toggle="modal" data-target=".bs-example-modal-sm">
+											<i class="fa fa-trash" aria-hidden="true"></i>
+										</a>
+									</td>
+								</tr>
+								<?php $count++; ?>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+					<!-- End of table -->
+				</div>
+				<!-- End of content -->
+			</div>
+			<!-- End of danger -->
+		</div>
+		<!-- End of col-md-6 -->
+	</div>
+	<!-- End of row -->
+</section>
+<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+  <div class="modal-dialog modal-sm" role="document">
+    <div class="modal-content">
+      ...
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('.table').DataTable();
+	});
+
+	// Detroy modal
+	$('body').on('hidden.bs.modal', '.modal', function () {
+		$(this).removeData('bs.modal');
+	}); 
+</script>
