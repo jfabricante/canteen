e21806a6b6598c080ebde5efdbd99ce9ae8f1ebb
Jerome <jerome-fabricante@isuzuphil.com>
2017-09-08 13:10:44 +0800

    Remove modal header

--- application/views/transaction/index_view.php
+++ application/views/transaction/index_view.php
@@ -343,9 +343,6 @@
 		<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" id="modal">
 			<div class="modal-dialog modal-sm" role="document">
 				<div class="modal-content">
-					<div class="modal-header">
-						<h4 class="modal-title">Isuzu Philippines Receipt</h4>
-					</div>
 
 					<div class="modal-body">
 						<!-- Item table -->
