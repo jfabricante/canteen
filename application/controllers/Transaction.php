767fa6c437276c731edf8dff9e1cf598f2dd9b8c
Jerome <jerome-fabricante@isuzuphil.com>
2017-09-08 13:17:20 +0800

    Generate receipt after the transaction was made

--- application/controllers/Transaction.php
+++ application/controllers/Transaction.php
@@ -39,6 +39,63 @@ class Transaction extends CI_Controller {
 		echo '<pre>';
 		print_r($data);
 		echo '</pre>';
+
+	protected function _generate_receipt($params)
+	{
+		try {
+			$connector = new WindowsPrintConnector('smb://BARCODE08/EPSON TM-T82II Receipt');
+			$printer = new Printer($connector);
+
+			$printer->initialize();
+
+			$copies = array("***Customer Copy***", "***Cashier Copy***");
+
+			foreach ($copies as $row) 
+			{
+				$printer->text("Isuzu Philippines Corporation\n");
+				$printer->text("Transaction#: " . $params['trans_id'] . "\n");
+				$printer->text(date('D, M d, Y h:i A') . "\n");
+				$printer->text(str_pad('', 48, '-'));
+				$printer->feed(1);
+
+				$printer->text("Customer: " . ucwords(strtolower($params['employee']['fullname'])) . "\n");
+				$printer->text("Meal Allowance: " . $params['employee']['allowance'] . "\n");
+				$printer->text("Cashier: " . ucwords(strtolower($this->session->userdata('fullname'))) . "\n");
+				$printer->text(str_pad('', 48, '-'));
+				$printer->feed(1);
+
+				$printer->text("Purchased Items\n");
+
+				foreach ($params['cart'] as $item) 
+				{
+					$printer->text(str_pad($item['name'], 32));
+					$printer->text(str_pad($item['quantity'] . 'x' , 6));
+					$printer->text(str_pad($item['total'], 10) . "\n");
+				}
+				$printer->text(str_pad('', 48, '-'));
+				$printer->feed(1);
+
+				$printer->text(str_pad("Total:", 38) . str_pad($params['totalPurchase'], 10) . "\n");
+				//$printer->text(str_pad("Credit:", 38) . str_pad($params['remaining_credit'], 10) . "\n");
+				$printer->text(str_pad("Credit:", 38) . str_pad($params['credit_used'], 10) . "\n");
+				$printer->text(str_pad("Cash:", 38) . str_pad($params['cash'], 10) . "\n");
+				$printer->text(str_pad("Change:", 38) . str_pad($params['change'], 10) . "\n");
+				$printer->feed(2);
+				$printer->text(str_pad('', 48, '_') . "\n");
+				$printer->setJustification(Printer::JUSTIFY_CENTER);
+				$printer->text("SIGNATURE\n");
+				$printer->text($row . "\n");
+				$printer->feed();
+				$printer->setJustification();
+				$printer->cut();
+			}
+			
+			$printer->close();
+
+
+		} catch(Exception $e) {
+		    echo "Couldn't print to this printer: " . $e->getMessage() . "\n";
+		}
 	}
 
 }
