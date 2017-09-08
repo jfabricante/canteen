fc39d8cb8e9c55be398a31435425166899fb4656
Jerome <jerome-fabricante@isuzuphil.com>
2017-09-08 13:01:56 +0800

    Store transaction resource

--- application/models/Transaction_model.php
+++ application/models/Transaction_model.php
@@ -8,4 +8,28 @@ class Transaction_model extends CI_Model {
 
 		$this->load->database();
 	}
+
+	public function store($params)
+	{
+
+		if (count($params['cart']) > 0)
+		{
+			$config = array(
+					'user_id'        => $params['employee'] ? $params['employee']['id'] : 0,
+					'credit_used'    => $params['credit_used'],
+					'cash'           => $params['cash'],
+					'total_purchase' => $params['totalPurchase'],
+					'change'         => $params['change'],
+					'cashier_id'     => $this->session->userdata('id'),
+					'datetime'       => date('Y-m-d H:i:s')
+				);
+
+			$this->db->insert('transaction_tbl', $config);
+			
+			return $this->db->insert_id();
+		}
+
+		return 0;
+	}
+
 }
