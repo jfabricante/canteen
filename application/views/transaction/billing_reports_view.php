303bcc2b3ea3eb89b6cabafe930ef352928a7290
Jerome <jerome-fabricante@isuzuphil.com>
2017-09-14 14:58:46 +0800

    Add dompdf library

---
+++ application/views/transaction/billing_reports_view.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Canteen System Billing Reports</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="<?php echo base_url('resources/templates/bootstrap-3.3.7/css/bootstrap.min.css');?>" rel="stylesheet" >
+	<!-- Admin LTE core CSS -->
+	<link href="<?php echo base_url('resources/templates/AdminLTE-2.3.5/dist/css/AdminLTE.min.css');?>" rel="stylesheet" >
+	<link href="<?php echo base_url('resources/templates/AdminLTE-2.3.5/dist/css/skins/_all-skins.min.css');?>" rel="stylesheet" >
+	<!-- Custom styles for this template -->
+	<link href="<?php echo base_url('resources/css/custom.css');?>" rel="stylesheet" >
+
+</head>
+<body>
+	<!-- row -->
+	<div class="row">
+		<!-- col-md-12 -->
+		<div class="col-md-12">
+			<!-- box -->
+			<div class="box box-danger">
+				<!-- box-header -->
+				<div class="box-header with-border">
+					<h4 class="box-title">Billing Reports</h4>
+				</div>
+				<!-- /box-header -->
+
+				<!-- box-body -->
+				<div class="box-body">
+					<table class="table table-condensed table-striped table-bordered">
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Name</th>
+								<th>Date</th>
+								<th>Credit Used</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>Jerome Fabricante</td>
+								<td>September 14, 2017</td>
+								<td>1000</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<!-- /box-body -->
+			</div>
+			<!-- /box -->
+		</div>
+		<!-- /col-md-12 -->
+	</div>
+	<!-- /row -->
+</body>
+</html>
