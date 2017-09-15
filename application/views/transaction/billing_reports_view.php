54dfb0a967ac98679e8622fa7761463531635cbd
Jerome <jerome-fabricante@isuzuphil.com>
2017-09-15 09:50:51 +0800

    Remove content for testing

--- application/views/transaction/billing_reports_view.php
+++ application/views/transaction/billing_reports_view.php
@@ -11,45 +11,9 @@
 
 </head>
 <body>
-	<!-- row -->
-	<div class="row">
-		<!-- col-md-12 -->
-		<div class="col-md-12">
-			<!-- box -->
-			<div class="box box-danger">
-				<!-- box-header -->
-				<div class="box-header with-border">
-					<h4 class="box-title">Billing Reports</h4>
-				</div>
-				<!-- /box-header -->
 
-				<!-- box-body -->
-				<div class="box-body">
-					<table class="table table-condensed table-striped table-bordered">
-						<thead>
-							<tr>
-								<th>#</th>
-								<th>Name</th>
-								<th>Date</th>
-								<th>Credit Used</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>1</td>
-								<td>Jerome Fabricante</td>
-								<td>September 14, 2017</td>
-								<td>1000</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<!-- /box-body -->
 			</div>
-			<!-- /box -->
 		</div>
-		<!-- /col-md-12 -->
 	</div>
-	<!-- /row -->
 </body>
 </html>
