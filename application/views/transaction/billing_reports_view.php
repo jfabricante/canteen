22f77f090249300e4e94924f198db22f8f514e0d
Jerome <jerome-fabricante@isuzuphil.com>
2017-09-16 12:08:12 +0800

    html markup to be rendered to pdf

--- application/views/transaction/billing_reports_view.php
+++ application/views/transaction/billing_reports_view.php
@@ -39,67 +39,61 @@
 </head>
 <body>
 	<!-- box -->
-	<div class="box box-danger">
-		<!-- box-header -->
-		<div class="box-header with-border">
-			<h4 class="box-title"><?php echo isset($title) ? $title : '' ?></h4>
-		</div>
-		<!-- /box-header -->
-
-		<!-- box-body -->
-		<div class="box-body">
-			<!-- table -->
-			<table class="table table-condensed table-striped table-bordered">
-				<thead>
+	<div>
+		<!-- table -->
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Trans ID</th>
+					<th>Employee</th>
+					<th>Credit Used</th>
+					<th>Cash Used</th>
+					<th>Date</th>
+					<th>Cashier</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach($entities as $entity): ?>
 					<tr>
-						<th>Trans ID</th>
-						<th>Employee</th>
-						<th>Credit Used</th>
-						<th>Cash Used</th>
-						<th>Date</th>
-						<th>Cashier</th>
+						<td><?php echo $entity['id'] ?></td>
+						<td><?php echo $entity['employee']; ?></td>
+						<td><?php echo $entity['credit_used'] ? number_format($entity['credit_used'], 2) : ''; ?></td>
+						<td><?php echo $entity['cash'] ? number_format($entity['cash'], 2) : ''; ?></td>
+						<td><?php echo date('m/d/Y h:i A', strtotime($entity['datetime'])); ?></td>
+						<td><?php echo $entity['cashier']; ?></td>
 					</tr>
-				</thead>
-				<tbody>
-					<?php foreach($entities as $entity): ?>
-						<tr>
-							<td><?php echo $entity['id'] ?></td>
-							<td><?php echo $entity['employee']; ?></td>
-							<td><?php echo $entity['credit_used']; ?></td>
-							<td><?php echo $entity['cash']; ?></td>
-							<td><?php echo date('m/d/Y h:i A', strtotime($entity['datetime'])); ?></td>
-							<td><?php echo $entity['cashier']; ?></td>
-						</tr>
-					<?php endforeach; ?>
-				</tbody>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+		<!-- /table -->
+
+		<!-- total -->
+		<div class="total">
+			<h4>Total Bill Amount: <span class="total-bill"><?php echo number_format($total_bill, 2) ?></span></h4>
+		</div>
+		<!-- ./total -->
+
+		<!-- signature -->
+		<div class="signature">
+			<table>
+				<tr>
+					<td>
+						<div class="signature-item">
+							<p>Prepared by:</p>
+						</div>
+					</td>
+
+					<td>
+						<div class="signature-item">
+							<p>Checked by:</p>
+						</div>
+					</td>
+				</tr>
 			</table>
-			<!-- /table -->
-
-			<div class="total">
-				<h4>Total Bill: <span class="total-bill"><?php echo $total_bill ?></span></h4>
-			</div>
-
-			<div class="signature">
-				<table>
-					<tr>
-						<td>
-							<div class="signature-item">
-								<p>Prepared by:</p>
-							</div>
-						</td>
-
-						<td>
-							<div class="signature-item">
-								<p>Checked by:</p>
-							</div>
-						</td>
-					</tr>
-				</table>
-			</div>
-
 		</div>
-		<!-- /box-body -->
+		<!-- ./signature -->
 	</div>
 	<!-- /box -->
+
 </body>
 </html>
