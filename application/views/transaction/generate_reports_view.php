06fdc619d50f3b29afe039f732786931809e0037
Unknown <jerome-fabricante@isuzuphil.com>
2017-12-16 09:46:32 +0800

    Add parameter for creating different types of reports

--- application/views/transaction/generate_reports_view.php
+++ application/views/transaction/generate_reports_view.php
@@ -3,48 +3,57 @@
 <section class="content">
 	<div class="row">
 		<!-- col-md-6 -->
-		<div class="col-md-6">
+		<div class="col-md-10">
 			<?php echo $this->session->flashdata('message'); ?>
 			<div class="box box-danger">
 				<!-- box-header -->
-				<div class="box-header"></div>
-				<!-- /.box-header -->
-
-				<!-- box-body -->
-				<div class="box-body">
+				<div class="box-header with-border">
 					<!-- form -->
-					<form action="<?php echo base_url('index.php/transaction/handle_billing_report'); ?>" method="post">
+					<form action="<?php echo base_url('index.php/transaction/generate_billing_report'); ?>" method="post">
 						<!-- row -->
 						<div class="row">
-							<div class="col-md-4">
+							<div class="col-md-3">
 								<div class="form-group">
 									<div class="input-group date">
 										<span class="input-group-addon">From</span>
-										<input type="text" class="form-control datepicker" id="from" name="from" value="<?php echo date('m/d/Y') ?>" required>
-									</div>
-								</div>
-							</div>
-
-							<div class="col-md-4">
-								<div class="form-group">
-									<div class="input-group date">
-										<span class="input-group-addon">To</span>
-										<input type="text" class="form-control datepicker" id="to" name="to" value="<?php echo date('m/d/Y') ?>" required>
+										<input type="text" class="form-control datepicker" id="from" name="from" value="<?php echo isset($params['from']) ? $params['from'] :  date('m/d/Y') ?>" required>
 									</div>
 								</div>
 							</div>
 
 							<div class="col-md-3">
 								<div class="form-group">
+									<div class="input-group date">
+										<span class="input-group-addon">To</span>
+										<input type="text" class="form-control datepicker" id="to" name="to" value="<?php echo isset($params['to']) ? $params['to'] : date('m/d/Y') ?>" required>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-md-2">
+								<div class="form-group">
+									<button type="submit" class="btn btn-info btn-flat btn-block" name="filter_date">Filter Date</button>
+								</div>
+							</div>
+
+							<div class="col-md-2">
+								<div class="form-group">
+									<button type="submit" class="btn btn-info btn-warning btn-flat btn-block" name="create_invoice">Create Invoice</button>
+								</div>
+							</div>
+
+
+							<div class="col-md-2">
+								<div class="form-group">
 									<!-- pdf report -->
 									<button type="submit" class="btn btn-danger btn-flat" name="pdf_report">
-										<i class="fa fa-file-pdf-o fa-2x" aria-hidden="true"></i>
+										<i class="fa fa-file-pdf-o" aria-hidden="true"></i>
 									</button>
 									<!-- ./pdf report -->
 
 									<!-- excel report -->
 									<button type="submit" class="btn btn-success btn-flat" name="excel_report">
-										<i class="fa fa-file-excel-o fa-2x" aria-hidden="true"></i>
+										<i class="fa fa-file-excel-o" aria-hidden="true"></i>
 									</button>
 									<!-- ./excel report -->
 								</div>
