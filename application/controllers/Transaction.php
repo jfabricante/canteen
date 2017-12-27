a521bb232e805c6a3240123fb6e12479ad445ead
Unknown <jerome-fabricante@isuzuphil.com>
2017-12-27 09:43:28 +0800

    Connect to local printer

--- application/controllers/Transaction.php
+++ application/controllers/Transaction.php
@@ -67,7 +67,9 @@ class Transaction extends CI_Controller {
 	{
 		try {
 			// $connector = new WindowsPrintConnector('smb://BARCODE08/EPSON TM-T82II Receipt');
-			$connector = new WindowsPrintConnector('smb://IPCPC367/EPSON TM-T82II Receipt6');
+			// $connector = new WindowsPrintConnector('smb://IPCPC367/EPSON TM-T82II Receipt6');
+			// $connector = new WindowsPrintConnector('smb://localhost/EPSON TM-T82II Receipt6');
+			$connector = new WindowsPrintConnector('smb://localhost/EPSON TM-T82II Receipt');
 			$printer = new Printer($connector);
 
 			$printer->initialize();
